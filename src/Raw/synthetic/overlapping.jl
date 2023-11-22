2a24a57f5c53ec6835d641fd994ef18b3bb00679
kmariuszk <kiciorm@gmail.com>
2023-11-22 12:59:07 +0100

    Remove commented code in multi_class.jl and overlapping.jl

--- src/Raw/synthetic/overlapping.jl
+++ src/Raw/synthetic/overlapping.jl
@@ -4,10 +4,6 @@
 Loads overlapping synthetic data.
 """
 function load_overlapping_raw(n=250; seed=data_seed)
-    # counterfactual_data = load_blobs(n; seed=seed, centers=2, cluster_std=2.0)
-
-    # return counterfactual_data
-
     raw_data = load_blobs_raw(n; seed=seed, centers=2, cluster_std=2.0)
 
     return raw_data
