382bb26015ef42887db501a14c5cbb7789cfcd08
pat-alt <altmeyerpat@gmail.com>
2025-01-09 16:19:55 +0100

    vamos

--- src/synthetic/linearly_separable.jl
+++ src/synthetic/linearly_separable.jl
@@ -1,12 +1,12 @@
 """
-    load_linearly_separable(n=250)
+    load_linearly_separable(n=250; seed=data_seed)
 
 Loads linearly separable synthetic data.
 
 !!! note
-    This calls the [`load_blobs`](@ref) function with specific parameters and the seed set to [`data_seed`](@ref). The function returns the synthetic data with two clusters that are linearly separable. For more flexibility, you can use [`load_blobs`](@ref) directly with different parameters if needed.
+    This calls the [`load_blobs`](@ref) function with specific parameters and the seed set to [`data_seed`](@ref). To ensure linear spearability and reproducibility, setting the `seed` keyword argument has no effect. For more flexibility, you can use [`load_blobs`](@ref) directly with different parameters if needed.
 """
-function load_linearly_separable(n=250)
+function load_linearly_separable(n=250; seed=data_seed)
     data = load_blobs(n; seed=data_seed, centers=2, cluster_std=0.5)
     return data
 end
