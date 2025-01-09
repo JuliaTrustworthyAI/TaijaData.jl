6604605066531964fa6f37100a04150037299fbd
pat-alt <altmeyerpat@gmail.com>
2025-01-09 13:46:20 +0100

    that'll do

--- src/synthetic/circles.jl
+++ src/synthetic/circles.jl
@@ -5,7 +5,7 @@ Loads synthetic circles data.
 """
 function load_circles(n=250; seed=data_seed, noise=0.15, factor=0.01)
     if isa(seed, Int)
-        Random.seed!(rng, seed)
+        seed = Xoshiro(seed)
     end
 
     X, y = MLJBase.make_circles(n; rng=seed, noise=noise, factor=factor)
--- src/synthetic/moons.jl
+++ src/synthetic/moons.jl
@@ -5,7 +5,7 @@ Loads synthetic moons data.
 """
 function load_moons(n=250; seed=data_seed, kwrgs...)
     if isa(seed, Int)
-        Random.seed!(rng, seed)
+        seed = Xoshiro(seed)
     end
     X, y = MLJBase.make_moons(n; rng=seed, kwrgs...)
     X = permutedims(MLJBase.matrix(X))
