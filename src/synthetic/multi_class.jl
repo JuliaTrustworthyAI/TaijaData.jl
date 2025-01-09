966f5312a7e828d95e82a6980ff9eba269cf853e
pat-alt <altmeyerpat@gmail.com>
2025-01-09 16:09:40 +0100

    couple more things

--- src/synthetic/multi_class.jl
+++ src/synthetic/multi_class.jl
@@ -1,9 +1,12 @@
 """
-    load_multi_class(n=250; seed=data_seed)
+    load_multi_class(n=250; centers=4)
 
 Loads multi-class synthetic data.
+
+!!! note
+    This calls the [`load_blobs`](@ref) function with specific parameters and the seed set to [`data_seed`](@ref). The function returns the synthetic data with two clusters that are linearly separable. For more flexibility, you can use [`load_blobs`](@ref) directly with different parameters if needed.
 """
-function load_multi_class(n=250; seed=data_seed, centers=4)
-    data = load_blobs(n; seed=seed, centers=centers, cluster_std=0.5)
+function load_multi_class(n=250; centers=4)
+    data = load_blobs(n; seed=data_seed, centers=centers, cluster_std=0.5)
     return data
 end
--- src/synthetic/overlapping.jl
+++ src/synthetic/overlapping.jl
@@ -1,9 +1,12 @@
 """
-    load_overlapping(n=250; seed=data_seed)
+    load_overlapping(n=250)
 
 Loads overlapping synthetic data.
+
+!!! note
+    This calls the [`load_blobs`](@ref) function with specific parameters and the seed set to [`data_seed`](@ref). The function returns the synthetic data with two clusters that are linearly separable. For more flexibility, you can use [`load_blobs`](@ref) directly with different parameters if needed.
 """
-function load_overlapping(n=250; seed=data_seed)
-    data = load_blobs(n; seed=seed, centers=2, cluster_std=2.0)
+function load_overlapping(n=250)
+    data = load_blobs(n; seed=data_seed, centers=2, cluster_std=2.0)
     return data
 end
